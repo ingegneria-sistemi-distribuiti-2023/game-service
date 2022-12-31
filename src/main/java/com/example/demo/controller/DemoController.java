90b98c11e93dd6caf1c76f892dc4708b3d1d34b5
m.bongiovanni <m.bongiovanni@mongibello.tech>
2022-12-31 18:57:34 +0100

    improved comments added dto Dummy2Dto added Entity DummyData added Mapper service for DummyData

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,14 +3,19 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Dummy2Dto;
+import com.example.demo.mapper.DummyMapperService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-/*
- * @RestController is a convenience annotation that does nothing more than adding the @Controller and @ResponseBody annotations
+/**
+ * The controller (presentation layer, or port) is a protocol interface which exposes application functionality as RESTful web services.
+ * It should to that and nothing more.
+ *
+ * @RestController is a convenience annotation that does nothing more than adding the Controller and ResponseBody annotations
  * @RequestMapping maps HTTP requests to handler methods of MVC and REST controllers
  * @RequestParam binds a web request parameter to a method parameter
  * @PathVariable binds a URI template variable to a method parameter
@@ -20,11 +25,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @ExceptionHandler configures global handling for specific exceptions
  * @SessionAttributes makes model attributes available to the web session
  * @SessionAttribute retrieves a specific model attribute from the web session 
- * @Controller is a specialization of @Component that allows for implementation classes to be autodetected through classpath scanning
- * 
+ * @Controller is a specialization of Component that allows for implementation classes to be autodetected through classpath scanning
  */
 @RestController // @Controller + @ResponseBody
 public class DemoController {
+
+	@Autowired
+	private DummyMapperService dummyMapperService;
+
 	@RequestMapping(path = "/dummy1", method = RequestMethod.GET)
 	public String dummy1() {
 		return "dummy1";
@@ -34,5 +42,11 @@ public class DemoController {
 	@RequestMapping(path = "/dummy2", method = RequestMethod.POST)
 	public String dummy2(@RequestBody Dummy2Dto dummy2dto) {
 		return dummy2dto.getName() + " " + dummy2dto.getSurname();
+	}
+
+	// dummy all: get all the data from the database
+	@RequestMapping(path = "/dummyall", method = RequestMethod.GET)
+	public String dummyall() {
+		return dummyMapperService.getAllData().toString();
 	}
 }
--- src/main/java/com/example/demo/dto/Dummy2Dto.java
+++ src/main/java/com/example/demo/dto/Dummy2Dto.java
@@ -1,22 +1,25 @@
 package com.example.demo.dto;
 
-import java.util.Optional;
 
 /*
  * a DTO is a Data Transfer Object, a simple object that is used to transfer data between processes (e.g. between a client and a server)
  */
 public class Dummy2Dto {
+    private Integer userId;
     private String name;
     private String surname;
+    private Integer anInteger;
     
     public Dummy2Dto() {
         super();
     }
     
-    public Dummy2Dto(String name, Optional<String> surname) {
+    public Dummy2Dto( Integer userId, String name, String surname, Integer anInteger) {
         super();
+        this.userId = userId;
         this.name = name;
-        this.surname = surname.orElse("not provided");
+        this.surname = surname;
+        this.anInteger = anInteger;
     }
     
     public String getName() {
@@ -31,7 +34,30 @@ public class Dummy2Dto {
         return surname;
     }
     
-    public void setSurname(Optional<String> surname) {
-        this.surname = surname.orElse("not provided");
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAnInteger() {
+        return anInteger;
+    }
+
+    public void setAnInteger(Integer anInteger) {
+        this.anInteger = anInteger;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    // to string method override 
+    @Override
+    public String toString() {
+        return "Dummy2Dto [userId=" + userId + ", name=" + name + ", surname=" + surname + ", anInteger=" + anInteger
+                + "]";
     }
 }
