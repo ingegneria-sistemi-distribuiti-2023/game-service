5d4b50b227edf32895a24e25736e558834bc2ba4
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2023-01-08 14:31:34 +0100

    added propertiy for port and added annotation @EnableWebMvc

--- src/main/java/com/isd/game/GameServiceApplication.java
+++ src/main/java/com/isd/game/GameServiceApplication.java
@@ -2,8 +2,10 @@ package com.isd.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication // @Configuration + @EnableAutoConfiguration + @ComponentScan (scan all the components in the same package or below)
+@EnableWebMvc
 public class GameServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GameServiceApplication.class, args);
