07a37e20f655c8a94730113ca67484e077747e16
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2023-01-31 21:55:20 +0100

    Added comment (TODO: comment test)

--- src/main/java/com/isd/game/commons/error/CustomServiceException.java
+++ src/main/java/com/isd/game/commons/error/CustomServiceException.java
@@ -3,6 +3,9 @@ package com.isd.game.commons.error;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/*
+ * Custom Exception used to handle error on Request
+ */
 @Data
 @NoArgsConstructor
 public class CustomServiceException extends Exception {
